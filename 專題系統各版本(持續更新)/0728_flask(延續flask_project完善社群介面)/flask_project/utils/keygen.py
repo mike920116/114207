56c8f4d809eb593379094b8229a1d4fc4a5a0d95
朱羿安 <10856037@ntub.edu.tw>
2025-07-28 15:15:41 +0800

    將這個錯誤的資料夾以及資料夾底下內容移除

--- 專題系統各版本(持續更新)/0728_flask(延續flask_project完善社群介面)/flask_project/utils/keygen.py
+++
@@ -1,7 +0,0 @@
-from hashlib import pbkdf2_hmac
-
-def derive_key(password: str, salt: str) -> bytes:
-    """
-    使用 PBKDF2 從密碼與 salt 派生出 256-bit AES 金鑰
-    """
-    return pbkdf2_hmac('sha256', password.encode(), salt.encode(), 100_000, dklen=32)
